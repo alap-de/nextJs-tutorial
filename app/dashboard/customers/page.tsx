ad15f78875649a2772ca5b74359fa406c984ec34
Alap De <alap.de@gmail.com>
2023-11-24 22:34:10 +0530

    Implement customers page

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,9 +1,36 @@
 import { Metadata } from "next";
+import { Suspense } from "react";
+
+import Search from "@/app/ui/search";
+import { lusitana } from "@/app/ui/fonts";
+import Table from "@/app/ui/customers/table";
+import { CustomersTableSkeleton } from "@/app/ui/skeletons";
 
 export const metadata: Metadata = {
   title: "Customers",
 };
 
-export default function Page() {
-  return <p>Customers Page</p>;
+export default async function Page({
+  searchParams,
+}: {
+  searchParams?: {
+    query?: string;
+    page?: string;
+  };
+}) {
+  const query = searchParams?.query || "";
+
+  return (
+    <main>
+      <div className="w-full">
+        <h1 className={`${lusitana.className} mb-8 text-xl md:text-2xl`}>
+          Customers
+        </h1>
+        <Search placeholder="Search customers..." />
+        <Suspense key={query} fallback={<CustomersTableSkeleton />}>
+          <Table query={query} />
+        </Suspense>
+      </div>
+    </main>
+  );
 }
